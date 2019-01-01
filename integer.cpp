5dd2ad9f8707b03d593c8f924241dce102ad1791
GHKim <aass98998@snu.ac.kr>
2019-01-01 16:28:22 +0900

    initial commit

---
+++ integer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ghkim on 19. 1. 1.
+//
+
+#include "integer.hpp"
---
+++ integer.hpp
@@ -0,0 +1,16 @@
+//
+// Created by ghkim on 19. 1. 1.
+//
+
+#ifndef POLY_INTEGER_HPP
+#define POLY_INTEGER_HPP
+
+
+
+class integer {
+
+};
+
+
+
+#endif //POLY_INTEGER_HPP
